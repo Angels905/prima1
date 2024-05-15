4f10d62b0184ec6b89a4c375c64c956628f67eeb
angels905 <166813206+Angels905@users.noreply.github.com>
2024-05-15 10:44:48 +0200

    ajoute des apit

--- model/users.js
+++ model/users.js
@@ -6,7 +6,6 @@ function getAllUsers() {
 
 }
 
-
  function getUser(id){
      return prisma.livre.findUnique( {where : {id}})//findUnique recuper uniquement une donneé 
  }
@@ -15,8 +14,29 @@ function getUserByNom_livre(nom_livre){//recupere un element pas le nom
     return prisma.livre.findUnique( {where : {nom_livre}})
 }
 
+//requete delete compte
+function deletcompte(id){//supprime par id
+    return prisma.compte.delete( {where : {id}})
+}
+
+//rée compte
+function addCompte(compte){
+    return prisma.compte.create( {data: compte})
+}
+
+//modifier livre
+function updateLivre(livre){
+    return prisma.livre.update( {
+        where:{id: UserActivation.id},
+        data: livre
+    })
+}
+
 module.exports = {
     getAllUsers,
     getUser,//affiche 
-    getUserByNom_livre
+    getUserByNom_livre,
+    addCompte,
+    deletcompte,//apple de la fonction
+    updateLivre
 }
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
   
-const {getAllUsers, getUser, getUserByNom_livre} = require('../model/users.js')// ajoute les nom des function defini
+const {getAllUsers, getUser, getUserByNom_livre, deletcompte,addCompte,updateLivre} = require('../model/users.js')// ajoute les nom des function defini
  
 /**
- * getall users 
+ * getall livre
 */ 
  
  router.get('/', function(req, res, next) {
@@ -12,14 +12,47 @@ const {getAllUsers, getUser, getUserByNom_livre} = require('../model/users.js')/
  
  });
 
-router.get('/:id ([0-9]+)', function(req, res, next) {//chaque fuction    un router
-    getUser(+req.params.id).then(user =>res.json(users ))//ne pas oublie le + pour les entiers
+// router.get('/:id ([0-9]+)', function(req, res, next) {//chaque fuction    un router
+//     getUser(+req.params.id).then(user =>res.json(users ))//ne pas oublie le + pour les entiers
  
-}); 
+// }); 
 
-router.get('/:nom_livre', function(req, res, next) {
-    getUserByNom_livre(req.params.nom_livre).then(user =>res.json(users)) 
-});
+// router.get('/:nom_livre', function(req, res, next) {
+//     getUserByNom_livre(req.params.nom_livre).then(user =>res.json(users)) 
+// });
+
+/**
+ * crée compte
+*/ 
+router.post('/', function(req, res, next) {
+    addCompte(req.body).then(user =>res.json(user)) 
+   });
+
+/** 
+ * delete compte
+*/ 
+
+router.delete('/:id', function(req, res, next) {
+     deletcompte(+req.params.id).then(user =>res.json(user)) 
+    });
+
+/**
+ * modifie livre
+*/ 
+
+router.patch('/:id', function(req, res, next) {
+    updateLivre(req.body).then(user =>res.json(user)) 
+   });
+
+// /**
+//  * modifie catalogue
+// */ 
+
+// router.patch('/:id', function(req, res, next) {
+//     updateCatalogue(req.body).then(user =>res.json(user)) 
+//    });
+
+
 
 module.exports = router;
    
